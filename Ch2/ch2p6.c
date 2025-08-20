fc5473632db33fdeee41be0ae3a9135faa478723
Javier Celestino <francisco.j.celestino@gmail.com>
2025-08-20 17:25:34 -0600

    Completed program 6 in Chapter 2.

---
+++ Ch2/ch2p6.c
@@ -0,0 +1,25 @@
+// Javier Celestino
+// C Programming - A Modern Approach Exercises
+// This program asks the user to enter a value for x
+// and then displays the value of the following polynomial:
+// (((((3x + 2) x - 5) x - 1) x + 7) x - 6)
+// This highlights Horner's Rule
+
+#include <stdio.h>
+
+int main(void) {
+    printf("This program computes the following polyomial highlighting Horner's Rule:\n");
+    printf("   (((((3x + 2) x - 5) x - 1) x + 7) x - 6)\n");
+    printf("Which x integer do you want to solve for? ");
+
+    long long x;
+
+    scanf("%lld", &x);
+
+    long long solution = ((((((3 * x) + 2) * x - 5) * x - 1) * x + 7) * x - 6);
+
+    printf("Replacing x with %lld, the solution is %lld\n", x, solution);
+    printf("Congratulations! Now you know Horner's Rule!\n");
+
+    return 0;
+}
