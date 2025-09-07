693f8d35a8075eb28498cad3aec8e9ab91951d7c
Javier Celestino <francisco.j.celestino@gmail.com>
2025-09-07 15:28:30 -0600

    Completed program 7 in Chapter 5.

---
+++ Ch5/ch5p7.c
@@ -0,0 +1,44 @@
+// Javier Celestino
+// C Programming - A Modern Approach Exercises
+//
+// This program finds the largest and smallest of four integers entered by the user.
+
+#include <stdio.h>
+
+int main(void) {
+    printf("Welcome! This program finds the largest and smallest of four integers.\n");
+    printf("Enter four integers: ");
+
+    int a, b, c, d;
+    scanf("%d %d %d %d", &a, &b, &c, &d);
+
+    int min1 = a, max1 = b;
+    int min2 = c, max2 = d;
+    int smallest, largest;
+
+    if (a > b) {
+        min1 = b;
+        max1 = a;
+    }
+
+    if (c > d) {
+        min2 = d;
+        max2 = c;
+    }
+
+    smallest = min1;
+
+    if (min2 < smallest) {
+        smallest = min2;
+    }
+
+    largest = max1;
+
+    if (max2 > largest) {
+        largest = max2;
+    }
+
+    printf("Largest: %d\nSmallest: %d\n", largest, smallest);
+
+    return 0;
+}
